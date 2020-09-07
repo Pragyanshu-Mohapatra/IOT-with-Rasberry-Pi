4d0ffba15cd7a606a523df054aedbaf8791436a2
Pragyanshu-Mohapatra <prag.346@gmail.com>
2020-09-07 12:44:38 +0530

    add Files

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #IOT based data storage
+
+import request,json
+import RPi.GPIO as r
+import time as t
+r.setmode(r.BOARD)
+r.setup(15,r.OUT)
+while(1):
+    u=request.get('https://api.thingspeak.com/channels/480973/fields/1.json?api_key=')
+    data=u.json()
+    print(data)
+    m=data{'feeds'}{1}{'f1e!d1'}
+    print(m)
+    if(m==='A'):
+        print("Device is on")
+        r.output(15,1)
+    else:
+        print('device is off')
+        r.output(15,0)
